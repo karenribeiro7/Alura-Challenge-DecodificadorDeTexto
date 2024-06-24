6d9e93c4f659d5d0318cd7deccf93bc71694254d
karenribeiro7 <kevr@discente.ifpe.edu.br>
2024-06-24 02:03:33 -0300

    add: template html, folha de estilo, imagens e aplicação em js

---
+++ app.js
@@ -0,0 +1,121 @@
+let novoTexto = '';
+let textoOriginal = '';
+
+function criptografar() {
+    let texto = document.querySelector('textarea').value;
+
+    for (let i = 0; i < texto.length; i++) {
+        let letra = texto[i];
+        let codigo = '';
+
+        switch (letra) {
+            case 'a':
+                codigo = 'ai';
+                break;
+            case 'e':
+                codigo = 'enter';
+                break;
+            case 'i':
+                codigo = 'imes';
+                break;
+            case 'o':
+                codigo = 'ober';
+                break;
+            case 'u':
+                codigo = 'ufat';
+                break;
+            default:
+                codigo = letra; 
+                break;
+        }
+
+        novoTexto += codigo;        
+    }
+    limparTexto();
+
+    exibirResultado(novoTexto);
+}
+
+function limparTexto(){
+    document.querySelector('textarea').value = '';
+    document.querySelector('textarea').setAttribute('placeholder', 'Digite seu texto');
+
+}
+
+function exibirResultado(resultado) {
+    document.getElementById("textoResultado").innerText = resultado;
+    document.getElementById("subtitulo").style.display = "none";
+    document.getElementById("iconeResultado").style.display = "none";
+    mostrarBotaoCopiar(resultado);
+}
+
+
+function descriptografar() {
+    let novoTexto = document.querySelector('textarea').value;
+
+    let i = 0;
+
+    while (i < novoTexto.length) {
+        let codigo = '';
+
+        if (novoTexto.startsWith('ai', i)) {
+            codigo = 'ai';
+        } else if (novoTexto.startsWith('enter', i)) {
+            codigo = 'enter';
+        } else if (novoTexto.startsWith('imes', i)) {
+            codigo = 'imes';
+        } else if (novoTexto.startsWith('ober', i)) {
+            codigo = 'ober';
+        } else if (novoTexto.startsWith('ufat', i)) {
+            codigo = 'ufat';
+        } else {
+            textoOriginal += novoTexto[i];
+            i++;
+            continue;
+        }
+
+        switch (codigo) {
+            case 'ai':
+                textoOriginal += 'a';
+                break;
+            case 'enter':
+                textoOriginal += 'e';
+                break;
+            case 'imes':
+                textoOriginal += 'i';
+                break;
+            case 'ober':
+                textoOriginal += 'o';
+                break;
+            case 'ufat':
+                textoOriginal += 'u';
+                break;
+        }
+
+        i += codigo.length; 
+    }
+    limparTexto();  
+    exibirResultado(textoOriginal);       
+}
+
+function mostrarBotaoCopiar(resultado) {
+    if (resultado.trim() !== '') {
+        document.getElementById("copiar").disabled = false;
+        document.getElementById("copiar").innerText = "Copiar";
+    } else {
+        document.getElementById("copiar").disabled = true;
+    }
+}
+
+function copiarResultado() {
+    let elemento = document.getElementById("textoResultado");
+    let texto = elemento.innerText || elemento.textContent;
+    
+
+    navigator.clipboard.writeText(texto).then(() => {
+        document.getElementById("copiar").innerText = "Copiado!";
+        document.getElementById("copiar").disabled = true; 
+    }).catch(err => {
+        console.error('Error copying text: ', err);
+    });
+}
